ce80d43fe344980fe9df204505cf45865fd12404
Abinaya <144458877+Abinayav3@users.noreply.github.com>
2023-10-19 06:44:48 +0530

    Add files via upload

--- main (1).py
+++ main (1).py
@@ -1,9 +1,34 @@
-# leap year
-print("enter the year:")
-y = int(input())
-if y % 4 == 0 and y % 100 != 0:
-  print("/n it is a leap year")
-elif y % 400 == 0:
-  print("/n it is a leap year")
-else:
-  print("/n it is not a leap year")
+# Python3 program to calculate the CGPA
+# and CGPA percentage of a student
+def CgpaCalc(marks, n):
+
+  # Variable to store the grades in
+  # every subject
+  grade = [0] * n
+
+  # Variables to store CGPA and the
+  # sum of all the grades
+  Sum = 0
+
+  # Computing the grades
+  for i in range(n):
+    grade[i] = (marks[i] / 10)
+
+  # Computing the sum of grades
+  for i in range(n):
+    Sum += grade[i]
+
+  # Computing the CGPA
+  cgpa = Sum / n
+
+  return cgpa
+
+
+# Driver code
+n = 5
+marks = [90, 80, 70, 80, 90]
+
+cgpa = CgpaCalc(marks, n)
+
+print("CGPA = ", '%.1f' % cgpa)
+print("CGPA Percentage = ", '%.2f' % (cgpa * 9.5))
